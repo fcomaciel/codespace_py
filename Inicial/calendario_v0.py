f733129ed3466042591401444e98386123c912c0
Francisco Maciel <Fcomaciel@gmail.com>
2023-03-13 23:06:52 +0000

    Calendario V0

--- Inicial/calendario_v0.py
+++ Inicial/calendario_v0.py
@@ -1,7 +1,19 @@
 meses = ['jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 
-## print(meses)
+for mes in meses:
+    print('Mês {0} ->'.format(mes), end=' ')
 
-for i in range(0, len(meses)):
-    print(i)
-    
+    if mes in (meses[1]):
+        for dia in range(1, 29):
+            print(dia, end=' ')
+        print()
+
+    elif mes in (meses[3],meses[5],meses[8],meses[10]):
+        for dia in range(1, 31):
+            print(dia, end=' ')
+        print()
+
+    else:
+        for dia in range(1, 32):
+            print(dia, end=' ')
+        print()
